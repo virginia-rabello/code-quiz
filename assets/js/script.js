e74b2b43ade39108265b45f7b07495ffd9410bcf
Virginia <virginia.rabello2107@gmail.com>
2021-04-09 18:01:47 -0500

    timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,18 @@ var quizArray = [
         q:""
     }
 ];
+var startBtn = document.getElementById('start-button');
+
+function countDown(){
+var timeLeft = 75;    
+var timer = setInterval(function(){
+    document.getElementById("timer").innerHTML = "Time: " + timeLeft;
+    if(timeLeft === 0){
+        clearInterval(timer);
+        displayReport();
+    }
+    timeLeft--;
+}, 1000);
+}
+
+startBtn.onclick = countDown;
