95e8d051d006e5a85cce5bd5b91c3179ee44a8f6
Virginia <virginia.rabello2107@gmail.com>
2021-04-12 11:16:12 -0500

    high scores working

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,7 @@ var quizArray = [
         correctAnswer:'c'
     }
 ];
+var arrayScores = [];
 
 var timeLeft = 75;
 
@@ -53,33 +54,65 @@ var footer = document.querySelector('footer');
 
 var divScreen = document.querySelector("#start-screen");
 
+var startScreen = '<div id="start-screen" class="start-screen"><h1>Code Quiz Chalenge</h1><p>This is a code quiz chalenge. Answer the questions before the time runs out. Every time you answer incorrectly you will be penalized by 10 seconds. The seconds remaining after you are done will be your score.</p><button id="start-button" class="start-button" type="button">Start</button></div><footer></footer>'
+
+var form;
+
+var endLoop = false;
+
+var highScoresLink = document.getElementById("my-link");
+
+var header = document.querySelector('header');
+    
 var buttonHandler = function(event) {
+   event.preventDefault();
     var target = event.target;
     var button = document.getElementById(target.id);
-    if(button.id === "start-button"){
-        countDown();
-        displayQuestion(index);
-        index++;
-    }
-    else{   
-        checkAnswer(button);
-            if(index < quizArray.length){
+    if(button.type === "button"){
+        if(button.id === "start-button"){
+            countDown();
             displayQuestion(index);
-            
             index++;
-            }
-            else {
-                stopTimer = true;
-                score = timeLeft;
-                saveScore(score);
-             }
         }
-    }
-
+         else{   
+            checkAnswer(button);
+                if(index < quizArray.length){
+                    displayQuestion(index);
+                    index++;
+                }
+                else {
+                    stopTimer = true;
+                    score = timeLeft;
+                    saveScoreForm(score);
+                    footer.remove();
+                    timer.style.display = "none";
+                }
+            }
+        }
+        if(button.type === "submit"){
+            if(button.id === "submit"){
+            scoreStorage();
+            showHighScores();
+            }
+            if(button.id === "go-back"){
+                endLoop = false;
+                index = 0;
+                divScreen.innerHTML = startScreen;
+            }
+            if(button.id === "clean"){
+                localStorage.clear();
+                showHighScores();
+                endLoop = true;
+            }
+        }
+        if(button.id === "my-link"){
+           showHighScores();
+        }
+}
 function countDown(){
     var timer = setInterval(function(){
     document.getElementById('timer').innerHTML = 'Time: ' + timeLeft;
-    if(timeLeft === 0){
+    if(timeLeft <= 0){
         clearInterval(timer);
         footer.innerHTML = "<h1>GAME OVER!</h1>";
     }
@@ -98,8 +131,8 @@ function displayQuestion(i){
     btnC =' <button id="c" class="button" type="button">'+ quizArray[i].c + '</button>'
     btnD =' <button id="d" class="button" type="button">'+ quizArray[i].d + '</button>'
 
-   divScreen.innerHTML = '<div id = "question"><h1>'+ quizArray[i].q + "</h1><ul><li>"+ btnA + "</li><li>"+ btnB + "</li><li>"+ btnC + "</li><li>"+ btnD + "</li></ul></div>";
-   divScreen = document.querySelector("#question");
+   divScreen.innerHTML = '<div id = "start-screen"><h1>'+ quizArray[i].q + "</h1><ul><li>"+ btnA + "</li><li>"+ btnB + "</li><li>"+ btnC + "</li><li>"+ btnD + "</li></ul></div>";
+   a
 }
 
 function checkAnswer(answer){
@@ -111,14 +144,44 @@ function checkAnswer(answer){
     else if(answer.id === quizArray[index-1].correctAnswer){
         footer.innerHTML = "<h1>CORRECT!</h1>";
     }
+     
+}
+
+function saveScoreForm(num){
+    var submitButton = '<button id="submit" class="start-button" type="submit" />';
+    var name = '<input id ="inicials" type="text" name="name" placeholder="Enter you inicials" />'
+    divScreen.innerHTML = '<div id = "submit-score"><h1>All Done!</h1><h3>Your final Score is '+ num +'!</h3><h3>Enter inicials:'+ name + '</h3>' + submitButton + 'Submit</button></div>' 
+    
+}
+function scoreStorage (){
+    var scoreEl = {
+        score:0,
+        inicials:"",
+    }
+    var inicials = document.getElementById("inicials").value;
+    scoreEl.score = score;
+    scoreEl.inicials = inicials;
+    arrayScores.push(scoreEl);
+    localStorage.setItem("scores",JSON.stringify(arrayScores));
    
 }
 
-function saveScore(num){
-    var submitButton = '<button id="submit" class="start-button" type="submit" />';
-    var name = '<input type="text" name="name" placeholder="Enter you inicials" />'
-    divScreen.innerHTML = '<div id = "submit-score"><h1>All Done!</h1><h3>Your final Score is '+ score +'!</h3><h3>Enter inicials:'+ name + '</h3>' + submitButton + 'Submit</button></div>' 
+function showHighScores(){
+    var goBackButton = '<button id="go-back" class="start-button" type="submit" />';
+    var cleanScores = '<button id="clean" class="start-button" type="submit" />';
+    var tableScores = '<table><tr><th>Name Inicials</th><th>Score</th></tr>';
+    var readScores = localStorage.getItem("scores");
+    var parseScores = JSON.parse(readScores);
+    var tableEls = "";
+    if(readScores){
+    parseScores.forEach(element => {
+    tableEls = tableEls + "<tr><td>"+ element.inicials + "</td><td>" + element.score + "</td></tr>"  
+    });
 }
- 
+    divScreen.innerHTML = '<div id = "high-scores"><h1>High Scores</h1>'+ tableScores + tableEls +'</div>'+ goBackButton + 'Go Back</button>' + cleanScores + 'Clean Scores</button>';
+}
+
+if(endLoop === false){
     divScreen.addEventListener("click",buttonHandler);
-     
+    header.addEventListener("click", buttonHandler);
+}
